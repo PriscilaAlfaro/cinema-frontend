8a4eebd3cbcc9a4056d1404ebec4079600d667c0
Priscila Alfaro <palfaro@Davids-MBP.lan>
2020-11-07 20:16:30 +0100

    fake initial test

--- src/Components/App/_tests_/App.test.js
+++ src/Components/App/_tests_/App.test.js
@@ -1,8 +1,9 @@
 // import { render, screen } from "@testing-library/react";
-// import App from "./App";
+// import App from "../App";
 
+test.todo("renders app Home page");
 // test("renders learn react link", () => {
 //   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
+//   const linkElement = screen.getByText(/Cinema CR/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
